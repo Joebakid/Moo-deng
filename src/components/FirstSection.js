c1730f75c59439110c2cd7a1fdd7d604740e1703
joebakid <josephbawo@gmail.com>
2024-10-01 00:54:54 +0100

    text correction

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -10,8 +10,8 @@ function FirstSection() {
   }
   return (
     <section className="container-section h-screen">
-      <div className="  items-center justify-center md:py-10">
-        <h1 className="  text-white  lg:text-7xl text-5xl  font-bold  text-center py-6 ">
+      <div className="  items-center justify-center py-3 md:py-10">
+        <h1 className="  text-white  lg:text-7xl text-6xl  font-bold  text-center py-6 ">
           Moodeng: The Hippo Meme Coin
         </h1>
         <h3 className="text-white text-2xl lg:5xl md:py-16  font-bold text-center">
