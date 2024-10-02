1ebd8a0b12a413df07d86242d6084d22eeac04c2
joebakid <josephbawo@gmail.com>
2024-10-02 13:14:27 +0100

    text correcton

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -13,10 +13,10 @@ function FirstSection() {
     <header id="home" className="  h-full ">
       <div className=" flex flex-col items-center justify-center py-24   md:w-[65%] mx-auto w-[80%]">
         <h1 className="  text-white  py-8  md:text-6xl   text-4xl     font-bold  text-center  2xl:text-9xl  ">
-          Moodeng: The Hippo MemeCoin
+          MooDeng: The Hippo MemeCoin
         </h1>
         <h3 className="  md:text-2xl py-8 text-xl  font-bold text-center text-yellow-400  2xl:text-6xl">
-          Dive into the whimsical world of Moodeng, the hippo-themed meme coin
+          Dive into the whimsical world of MooDeng, the hippo-themed meme coin
           that's making waves on the Dogecoin blockchain!
         </h3>
 
